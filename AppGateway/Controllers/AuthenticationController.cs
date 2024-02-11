d00ae301fc543e35a1f8556e1d2295275e87945c
Evgeny Shmelkov <evgeny.shmelkov@auxo.team>
2024-02-11 18:44:24 +0300

    changes. added gateway

---
+++ AppGateway/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portal.Gateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("login")]
+        public async Task Login(string returnUrl = "/")
+        {
+            await this.HttpContext.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme,
+                new AuthenticationProperties { RedirectUri = returnUrl });
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout(string returnUrl = "/")
+        {
+            return new SignOutResult(
+            new[] {
+                OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            }, new AuthenticationProperties { RedirectUri = returnUrl });
+        }
+    }
+}
